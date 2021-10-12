1c0a06acfa712fb0640527b4a98ec419195b125b
kiykoD <92391932+kiykoD@users.noreply.github.com>
2021-10-12 21:06:29 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,239 @@
+
+	 /* Reviews: https://kenwheeler.github.io/slick/*/
+	 
+	 let slider = $("#reviewsSlider");
+	 
+	 slider.slick({
+     infinite: true,
+     slidesToShow: 1,  
+     slidesToScroll: 1,
+	 fade: true,
+	 arrows: false,
+	 dots: true
+   });
+   
+         /*----presentation-----*/
+		 
+    document.addEventListener("DOMContentLoaded", function () {
+      var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+      console.log(scrollbar);
+      document.querySelector('[href="#openModal-1"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-1').style.marginLeft = scrollbar;
+      });
+      document.querySelector('[href="#openModal-2"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-2').style.marginLeft = scrollbar;
+      });
+      document.querySelector('[href="#openModal-3"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-3').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-4"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-4').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-5"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-5').style.marginLeft = scrollbar;
+      });
+	   document.querySelector('[href="#openModal-6"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-6').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-11"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-11').style.marginLeft = scrollbar;
+      }); 
+	  document.querySelector('[href="#openModal-21"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-21').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-22"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-22').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-23"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-23').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-24"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-24').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-25"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-25').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-26"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-26').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-27"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-27').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-28"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-28').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-29"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-29').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-30"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-30').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-31"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-31').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-32"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-32').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-33"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-33').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-34"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-34').style.marginLeft = scrollbar;
+      });
+	   document.querySelector('[href="#openModal-35"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-35').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-36"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-36').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-37"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-37').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-38"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-38').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-39"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-39').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-40"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-40').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-41"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-41').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-42"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-42').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-43"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-43').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-44"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-44').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-45"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-45').style.marginLeft = scrollbar;
+      });
+	   document.querySelector('[href="#openModal-46"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-46').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-47"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-47').style.marginLeft = scrollbar;
+      });
+	   document.querySelector('[href="#openModal-48"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-48').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-49"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-49').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-50"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-50').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-51"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-51').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-52"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-52').style.marginLeft = scrollbar;
+      });
+	   document.querySelector('[href="#openModal-53"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-53').style.marginLeft = scrollbar;
+      });
+	  	   document.querySelector('[href="#openModal-54"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-54').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-55"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-55').style.marginLeft = scrollbar;
+      });
+	  document.querySelector('[href="#openModal-56"]').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal-56').style.marginLeft = scrollbar;
+      });
+      document.querySelector('[href="#close"]').addEventListener('click', function () {
+        document.body.style.overflow = 'visible';
+        document.querySelector('#openModal-1').style.marginLeft = '0px';
+        document.querySelector('#openModal-2').style.marginLeft = '0px';
+        document.querySelector('#openModal-3').style.marginLeft = '0px';
+		document.querySelector('#openModal-4').style.marginLeft = '0px';
+		document.querySelector('#openModal-5').style.marginLeft = '0px';
+		document.querySelector('#openModal-6').style.marginLeft = '0px';
+		document.querySelector('#openModal-11').style.marginLeft = '0px';
+		document.querySelector('#openModal-21').style.marginLeft = '0px';
+		document.querySelector('#openModal-22').style.marginLeft = '0px';
+		document.querySelector('#openModal-23').style.marginLeft = '0px';
+		document.querySelector('#openModal-24').style.marginLeft = '0px';
+		document.querySelector('#openModal-25').style.marginLeft = '0px';
+		document.querySelector('#openModal-26').style.marginLeft = '0px';
+		document.querySelector('#openModal-27').style.marginLeft = '0px';
+		document.querySelector('#openModal-28').style.marginLeft = '0px';
+		document.querySelector('#openModal-29').style.marginLeft = '0px';
+		document.querySelector('#openModal-30').style.marginLeft = '0px';
+		document.querySelector('#openModal-31').style.marginLeft = '0px';
+		document.querySelector('#openModal-32').style.marginLeft = '0px';
+		document.querySelector('#openModal-33').style.marginLeft = '0px';
+		document.querySelector('#openModal-34').style.marginLeft = '0px';
+		document.querySelector('#openModal-35').style.marginLeft = '0px';
+		document.querySelector('#openModal-36').style.marginLeft = '0px';
+		document.querySelector('#openModal-37').style.marginLeft = '0px';
+		document.querySelector('#openModal-38').style.marginLeft = '0px';
+		document.querySelector('#openModal-39').style.marginLeft = '0px';
+		document.querySelector('#openModal-40').style.marginLeft = '0px';
+		document.querySelector('#openModal-41').style.marginLeft = '0px';
+		document.querySelector('#openModal-42').style.marginLeft = '0px';
+		document.querySelector('#openModal-43').style.marginLeft = '0px';
+		document.querySelector('#openModal-44').style.marginLeft = '0px';
+		document.querySelector('#openModal-45').style.marginLeft = '0px';
+		document.querySelector('#openModal-46').style.marginLeft = '0px';
+		document.querySelector('#openModal-47').style.marginLeft = '0px';
+		document.querySelector('#openModal-48').style.marginLeft = '0px';
+		document.querySelector('#openModal-49').style.marginLeft = '0px';
+		document.querySelector('#openModal-50').style.marginLeft = '0px';
+		document.querySelector('#openModal-51').style.marginLeft = '0px';
+		document.querySelector('#openModal-52').style.marginLeft = '0px';
+		document.querySelector('#openModal-53').style.marginLeft = '0px';
+		document.querySelector('#openModal-54').style.marginLeft = '0px';
+		document.querySelector('#openModal-55').style.marginLeft = '0px';
+		document.querySelector('#openModal-56').style.marginLeft = '0px';
+      });
+    });
+
